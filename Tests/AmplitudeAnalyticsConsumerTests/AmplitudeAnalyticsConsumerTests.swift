27664d9477d6e406af9509a5625dd746ae43ab7a
Tataru Robert <robert.tataru98@gmail.com>
2025-03-24 23:17:27 +0200

    Update Name

--- Tests/AmplitudeAnalyticsConsumerTests/AmplitudeAnalyticsConsumerTests.swift
+++ Tests/AmplitudeAnalyticsConsumerTests/AmplitudeAnalyticsConsumerTests.swift
@@ -23,7 +23,7 @@ SOFTWARE.
 */
 
 import Testing
-@testable import TAAnalyticsAmplitudeConsumer
+@testable import AmplitudeAnalyticsConsumer
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
